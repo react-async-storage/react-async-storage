354f22b7a230f5cbcd320f1bcf3b88df2af5859d
Na'aman Hirschfeld <Naaman.Hirschfeld@sprylab.com>
2020-11-29 22:59:38 +0100

    increased coverage

--- __tests__/provider.spec.tsx
+++ __tests__/provider.spec.tsx
@@ -4,29 +4,34 @@ import {
     CacheProvider,
     CacheWrapper,
     DEFAULTS,
+    dropCacheInstance,
     useCache,
 } from '../src'
 import { act, render } from '@testing-library/react'
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 
 const TEST_ID = 'cacheStoreName'
 
 const NestedComponent = (props: {
     isLoaded: boolean
-    storeName?: string
+    storeName?: string | string[]
     cb: (wrappers: any) => void
 }): React.ReactElement => {
-    let cache: CacheWrapper | undefined
+    let caches: CacheWrapper[] | undefined
     if (props.isLoaded) {
-        cache = useCache(props.storeName)
-        props.cb(cache)
+        caches = useCache(props.storeName ?? undefined)
+        props.cb(caches)
     }
-    return <div data-testid={TEST_ID}>{!!cache && cache.storeName}</div>
+    return (
+        <div data-testid={TEST_ID}>
+            {caches?.map((cache) => cache.storeName).join(',')}
+        </div>
+    )
 }
 const TestWrapper = (props: {
     configs?: CacheOptions | CacheOptions[]
     cb: (wrappers: any) => void
-    storeName?: string
+    storeName?: string | string[]
 }): React.ReactElement => {
     const [isLoaded, setIsLoaded] = useState(false)
     return (
@@ -36,7 +41,7 @@ const TestWrapper = (props: {
         >
             <NestedComponent
                 cb={props.cb}
-                storeName={props.storeName}
+                storeName={props.storeName ?? undefined}
                 isLoaded={isLoaded}
             />
         </CacheProvider>
@@ -44,14 +49,65 @@ const TestWrapper = (props: {
 }
 
 describe('Provider tests', () => {
+    const customName = 'customName'
     it('creates a default instance correctly', async () => {
-        let value: CacheWrapper | null = null
-        const cb = (wrapper: CacheWrapper) => {
-            value = wrapper
+        let wrappers: CacheWrapper[]
+        const cb = (val: CacheWrapper[]) => {
+            wrappers = val
         }
         const { getByTestId } = render(<TestWrapper cb={cb} />)
         await act(async () => Promise.resolve())
-        expect(value).toBeInstanceOf(CacheWrapper)
+        //@ts-ignore
+        for (const wrapper of wrappers) {
+            expect(wrapper).toBeInstanceOf(CacheWrapper)
+        }
         expect(getByTestId(TEST_ID)).toHaveTextContent(DEFAULTS.STORE_NAME)
+        await dropCacheInstance()
+    })
+    it('creates a custom instance correctly', async () => {
+        let wrappers: CacheWrapper[]
+        const cb = (val: CacheWrapper[]) => {
+            wrappers = val
+        }
+        const { getByTestId } = render(
+            <TestWrapper
+                cb={cb}
+                storeName={customName}
+                configs={[{ storeName: customName }]}
+            />,
+        )
+        await act(async () => Promise.resolve())
+        //@ts-ignore
+        for (const wrapper of wrappers) {
+            expect(wrapper).toBeInstanceOf(CacheWrapper)
+        }
+        expect(getByTestId(TEST_ID)).toHaveTextContent(customName)
+        await dropCacheInstance(customName)
+    })
+    it('creates multiple custom instances correctly', async () => {
+        let wrappers: CacheWrapper[]
+        const cb = (val: CacheWrapper[]) => {
+            wrappers = val
+        }
+        const { getByTestId } = render(
+            <TestWrapper
+                cb={cb}
+                storeName={[customName, 'customName2', 'customName3']}
+                configs={[
+                    { storeName: customName },
+                    { storeName: 'customName2' },
+                    { storeName: 'customName3' },
+                ]}
+            />,
+        )
+        await act(async () => Promise.resolve())
+        //@ts-ignore
+        for (const wrapper of wrappers) {
+            expect(wrapper).toBeInstanceOf(CacheWrapper)
+        }
+        expect(getByTestId(TEST_ID)).toHaveTextContent(
+            [customName, 'customName2', 'customName3'].join(','),
+        )
+        await dropCacheInstance(customName)
     })
 })
--- src/hooks.ts
+++ src/hooks.ts
@@ -4,10 +4,15 @@ import { DEFAULTS } from './constants'
 import { ValueError } from './errors'
 import { useContext } from 'react'
 
-export function useCache(storeName = DEFAULTS.STORE_NAME): CacheWrapper {
+export function useCache(
+    stores: string | string[] = [DEFAULTS.STORE_NAME],
+): CacheWrapper[] {
     const context = useContext(CacheContext)
-    if (!Reflect.has(context, storeName)) {
-        throw new ValueError(`invalid storeName ${storeName}`)
-    }
-    return context[storeName]
+    const storesArr = Array.isArray(stores) ? stores : [stores]
+    storesArr.forEach((name: string) => {
+        if (!Reflect.has(context, name)) {
+            throw new ValueError(`invalid storeName ${name}`)
+        }
+    })
+    return storesArr.map((name: string) => context[name])
 }
