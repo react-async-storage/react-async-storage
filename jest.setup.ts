80e7cadda87bbf3980525c3a1250c2c207d78b9a
Na'aman Hirschfeld <Naaman.Hirschfeld@sprylab.com>
2020-12-31 17:04:51 +0100

    updated async storage driver

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 //@ts-ignore
-import mockAsyncStorage from '@react-native-community/async-storage/jest/async-storage-mock'
-jest.mock('@react-native-community/async-storage', () => mockAsyncStorage)
+import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock'
+jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage)
